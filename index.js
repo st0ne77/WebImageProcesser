7dae751f1b9ace09fc5e134044f2c2373b2b04ce
st0ne77 <951251531@qq.com>
2023-05-26 13:34:28 +0800

    canvas

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+function main () {
+    const canvas = document.getElementById('webgl')
+    const gl = canvas.getContext('webgl')
+    gl.clearColor(0.0, 0.0, 0.0, 1.0)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+}
+
+function loadImage() {
+
+}
